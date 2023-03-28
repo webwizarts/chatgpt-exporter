11fbedffb37c0521cbc1914ee6cfde8d54e5d436
unknown <akskarim17@gmail.com>
2023-03-28 06:04:25 +0600

    Filename is generating by page title

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,30 +17,38 @@ export const generateDownload = (
   URL.revokeObjectURL(url);
 };
 
-// TODO: Make filename timestamp.filetype
-export const downloadPDF = (content: string) => {
+const filename = () => {
+  const title = document.title
+  return title
+}
+
+export const downloadPDF = (content: any) => {
   html2pdf(content, {
     margin: 0,
-    filename: "prompt.pdf",
+    filename: filename() + '.pdf',
     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    pageBreak: {
+      mode: ['legacy', 'avoid-all', 'css']
+    },
+    enableLinks: true
   });
 };
 
 export const downloadMD = (content: string) => {
   const turndownService = new TurndownService();
   const markdown = turndownService.turndown(content);
-  generateDownload("prompt.md", markdown, "text/markdown");
+  generateDownload(filename() + ".md", markdown, "text/markdown");
 };
 
 export const downloadRTF = (content: string) => {
   const htmlToRtf = new HtmlToRtfBrowser();
   const rtf = htmlToRtf.convertHtmlToRtf(content);
-  generateDownload("prompt.rtf", rtf, "application/rtf;charset=utf-8");
+  generateDownload(filename() + ".rtf", rtf, "application/rtf;charset=utf-8");
 };
 
 export const downloadTXT = (content: string) => {
   const text = htmlToText(content, { wordwrap: 130 });
-  generateDownload("prompt.txt", text, "text/plain");
+  generateDownload(filename() + ".txt", text, "text/plain");
 };
 
 export const isValidUrl = (url: string) => {
