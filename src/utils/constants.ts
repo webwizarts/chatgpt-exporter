0033ca6aae6de59bd6955a554fc281928bf94cbc
Nahid Hossain <nahid@nahids-mbp.home>
2023-04-01 22:13:09 -0400

    updated constant for button container

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,7 +13,7 @@ export const CONTENT_CONTAINER = ".w-full.break-words.dark\\:prose-invert";
 
 // This is the classname group for selecting the button container
 export const BUTTON_CONTAINER =
-  ".text-gray-400.flex.self-end.lg\\:self-center.justify-center.mt-2.gap-3.md\\:gap-4.lg\\:gap-1.lg\\:absolute.lg\\:top-0.lg\\:translate-x-full.lg\\:right-0.lg\\:mt-0.lg\\:pl-2.visible";
+  ".text-gray-400.flex.self-end.lg\\:self-center.justify-center.mt-2.gap-2.md\\:gap-3.lg\\:gap-1.lg\\:absolute.lg\\:top-0.lg\\:translate-x-full.lg\\:right-0.lg\\:mt-0.lg\\:pl-2.visible";
 
 // This is the container for modal
 // Note: this does not have an ID
